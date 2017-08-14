714b76889c2e2a27d493fde273288f6e5e996c48
Estudiante <Estudiante@10.203.2.22>
2017-08-14 15:59:35 -0500

    Version 0 Ejemplo hola mundo

---
+++ src/javaapplication6/JavaApplication6.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication6;
+import java.util.Scanner; 
+
+/**
+ *
+ * @author Estudiante
+ */
+public class JavaApplication6 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static int funcion(int dato ){
+        int salida;
+        int positivo; 
+        positivo= dato * -1; 
+        salida = positivo; 
+        return salida; 
+    }
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int i = 0; 
+        int numero= 0; 
+        Scanner flujoEntrada = new Scanner(System.in);
+        flujoEntrada.nextInt(numero); 
+        
+        if (numero>0){
+             for( i, i>numero, i++){
+                 
+             }        
+        }else{
+            
+            
+        }
+        
+        
+    }
+    
+}
